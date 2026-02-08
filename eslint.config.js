04a6f705ac0cf54b66a915a780a0f5a61a0a5beb
EgorUser556 <loadend33@gmail.com>
2026-02-08 23:06:14 +0300

    chore: project setup

---
+++ eslint.config.js
@@ -0,0 +1,80 @@
+import path from "node:path";
+
+import { includeIgnoreFile } from "@eslint/compat";
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import { configs, plugins, rules } from "eslint-config-airbnb-extended";
+import { rules as prettierConfigRules } from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+const gitignorePath = path.resolve(".", ".gitignore");
+
+const jsConfig = defineConfig([
+  {
+    name: "js/config",
+    ...js.configs.recommended,
+  },
+  plugins.stylistic,
+  plugins.importX,
+  ...configs.base.recommended,
+  rules.base.importsStrict,
+]);
+
+const reactConfig = defineConfig([
+  plugins.react,
+  plugins.reactHooks,
+  plugins.reactA11y,
+  ...configs.react.recommended,
+  rules.react.strict,
+]);
+
+const typescriptConfig = defineConfig([
+  plugins.typescriptEslint,
+  ...configs.base.typescript,
+  rules.typescript.typescriptEslintStrict,
+  ...configs.react.typescript,
+]);
+
+const prettierConfig = defineConfig([
+  {
+    name: "prettier/plugin/config",
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+  {
+    name: "prettier/config",
+    rules: {
+      ...prettierConfigRules,
+      "prettier/prettier": "error",
+    },
+  },
+]);
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  ...jsConfig,
+  ...reactConfig,
+  ...typescriptConfig,
+  ...prettierConfig,
+  {
+    name: "simple-import-sort",
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  {
+    name: "project/overrides",
+    rules: {
+      "import-x/order": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+]);
