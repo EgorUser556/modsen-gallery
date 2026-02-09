5b669705b2109c66c63ab4bcd6c3fb19a6e278d2
EgorUser556 <loadend33@gmail.com>
2026-02-09 22:16:21 +0300

    feat: add images loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import './Loader.css';
+
+import React from 'react';
+
+const ImagesLoader: React.FC = () => (
+  <div className="images-loader">
+    <div className="images-loader__spinner" />
+  </div>
+);
+
+export default ImagesLoader;
